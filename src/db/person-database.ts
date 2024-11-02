94fd0744f7b384753a4d5d62db09cdabab34af4a
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-02 15:59:15 +0100

    add interface and all methods for createDb

--- src/db/person-database.ts
+++ src/db/person-database.ts
@@ -1,9 +1,43 @@
 import { Person } from "../validation";
 
-export const personDatabase: Person[] = [
-    {   id: "1",
-        name: "Anton",
-        personalNumber: "19921027-0196",
-        city: "Södertälje"
-    }
-];
+export interface PersonDatabase {
+  getAll: () => Promise<Person[]>;
+  addPerson: (person: Person) => Promise<void>;
+  getPersonById: (id: string) => Promise<Person | null>;
+  deletePersonById: (id: string) => Promise<boolean>;
+  putPersonById: (id: string, updatedPerson: Person) => Promise<Person | null>;
+}
+
+const createDb = (): PersonDatabase => {
+  const personDatabase: Person[] = [
+    {
+      id: "1",
+      name: "Anton",
+      personalNumber: "19921027-0196",
+      city: "Södertälje",
+    },
+  ];
+
+  return {
+    getAll: async () => personDatabase,
+    addPerson: async (person: Person) => {
+      personDatabase.push(person);
+    },
+    getPersonById: async (id: string) => {
+      const person = personDatabase.find((p) => p.id === id);
+      return person || null;
+    },
+    deletePersonById: async (id: string) => {
+      const index = personDatabase.findIndex((p) => p.id === id);
+      return index !== -1 ? (personDatabase.splice(index, 1), true) : false;
+    },
+    putPersonById: async (id: string, updatedPerson: Person) => {
+      const index = personDatabase.findIndex((p) => p.id === id);
+      if (index !== -1) {
+        personDatabase[index] = { ...updatedPerson, id };
+        return personDatabase[index];
+      }
+      return null;
+    },
+  };
+};
