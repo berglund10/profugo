37a73557c7f579a3fb3c8893c46d40b19070d634
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-02 15:09:39 +0100

    add registration router

---
+++ src/routes/registration.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import { v4 as uuidv4 } from 'uuid';
+
+export function createRegistrationRouter() {
+  return {
+    getRouter() {
+      const router = express.Router();
+      router.get("/", (req: Request, res: Response) => {
+        res.status(200).json([]);
+      });
+
+      router.post("/", (req: Request, res:Response) => {
+        const {name, personalNumber, city} = req.body;
+        const id = uuidv4();
+        const PersonToRegister = {
+            id,
+            name,
+            personalNumber,
+            city
+        }
+        //db.add(PersonToRegister)
+        res.status(201).json({id})
+      })
+
+      return router;
+    },
+  };
+}
