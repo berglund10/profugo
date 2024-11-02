c137da7f3b0c1f915816d38dfba0675e0007c0e7
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-02 21:38:35 +0100

    input can't contain more than 12 digits test

--- src/logic/luhnLogic.ts
+++ src/logic/luhnLogic.ts
@@ -2,5 +2,9 @@ export const isValidLuhn = (personalNumber: string) => {
 
     if(personalNumber.length === 0) return false;
 
+    if (/[^0-9]/.test(personalNumber)) {
+        return false;
+    }
+
     return true;
 }
