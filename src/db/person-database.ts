37508facba11e1df4d58890a29713d2581802727
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-02 15:39:45 +0100

    created mock database

---
+++ src/db/person-database.ts
@@ -0,0 +1,9 @@
+import { Person } from "../validation";
+
+export const personDatabase: Person[] = [
+    {   id: "1",
+        name: "Anton",
+        personalNumber: "19921027-0196",
+        city: "Södertälje"
+    }
+];
