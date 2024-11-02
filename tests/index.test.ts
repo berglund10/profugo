03b7f6f70ac81806dc6652b3e5d3f121c654c12f
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-02 14:50:19 +0100

    test for routes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -12,3 +12,47 @@ test("GET /status", async () => {
     deepEqual(result.status, 200)
     deepEqual(result.body, {message : "Ready"});
 })
+
+test("GET /api/v1/register", async () => {
+
+    const app = createApp();
+
+    const result = await request(app).get("/api/v1/register");
+
+    deepEqual(result.status, 200)
+    deepEqual(result.body, []);
+})
+test("POST /api/v1/register", async () => {
+
+    const app = createApp();
+
+    const result = await request(app).post("/api/v1/register").send({name: "Anton", personalNumber: "19921027", city: "Stockholm"});
+
+
+    deepEqual(result.status, 201)
+})
+test("GET /api/v1/register/:id", async () => {
+
+    const app = createApp();
+
+    const result = await request(app).get("/api/v1/register/1");
+
+    deepEqual(result.status, 200)
+})
+test("DELETE /api/v1/register", async () => {
+
+    const app = createApp();
+
+    const result = await request(app).delete("/api/v1/register/1");
+
+    deepEqual(result.status, 200)
+})
+
+test("PATCH /api/v1/register", async () => {
+
+    const app = createApp();
+
+    const result = await request(app).patch("/api/v1/register/1").send({city: "Södertälje"});
+
+    deepEqual(result.status, 200)
+})
