5f3d361f3672073ff19711cc129114681d3136c6
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-02 15:14:19 +0100

    add route for patch

--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -32,6 +32,12 @@ export function createRegistrationRouter() {
         res.status(200).json("Delete person by id:" + id);
       })
 
+      router.patch("/:id", (req: Request, res:Response) => {
+        const id = req.params.id;
+        const { city } = req.body;
+        res.status(200).json("Updated city for person" + id) 
+      })
+
       return router;
     },
   };
