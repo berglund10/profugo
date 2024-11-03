9bdda25dbaece20a43b3ff74bd3088f198c3de03
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-03 11:28:11 +0100

    add get and post method for contributions router

--- src/routes/food-contribution.ts
+++ src/routes/food-contribution.ts
@@ -1,18 +1,51 @@
 import express, { Request, Response } from "express";
+import { ContributionDatabase } from "../db/contributions-database";
+import { contributionSchema } from "../validation";
+import { v4 as uuidv4 } from "uuid";
 
-export function createFoodContributionsRouter() {
+export function createFoodContributionsRouter(db: ContributionDatabase) {
   return {
     getRouter() {
       const router = express.Router();
 
       router.get("/", async (req: Request, res: Response) => {
-        res.status(200).json("GET ALL")
+        try {
+          const contributions = await db.getAll();
+          res.status(200).json(contributions);
+        } catch (error) {
+          if (error instanceof Error) {
+            res.status(500).json({ error: error.message });
+            return;
+          }
+          res.status(500).json({ error: { message: "Internal server error" } });
+        }
       });
 
       router.post("/", async (req: Request, res: Response) => {
-        res.status(200).json("ADD Contributions to list")
-      });
+        const { name, location } = req.body;
+        const id = uuidv4();
+        const contributionToRegister = {
+          id,
+          name,
+          location,
+        };
+        const result = contributionSchema.safeParse(contributionToRegister)
+        if (!result.success) {
+          res.status(400).json({ error: { message: "Invalid input" } });
+          return;
+        }
 
+        try {
+          await db.addContribution(contributionToRegister);
+          res.status(201).json(contributionToRegister.id);
+        } catch (error) {
+          if (error instanceof Error) {
+            res.status(400).json({ error: error.message });
+            return;
+          }
+          res.status(500).json({ error: { message: "Internal server error" } });
+        }
+      });
 
       return router;
     },
