b088f1331fe558aeff53db88469ca467c8361e7d
Anton Berglund <antonberglund_92@hotmail.com>
2024-11-03 12:07:31 +0100

    updated controller, pass db referens once

--- src/controllers/registrationController.ts
+++ src/controllers/registrationController.ts
@@ -4,85 +4,58 @@ import { personSchema, updatePersonSchema } from "../validation";
 import { isValidLuhn } from "../logic/luhnLogic";
 import { v4 as uuidv4 } from "uuid";
 
-export const getPersons =
-  (db: PersonDatabase) => async (req: Request, res: Response) => {
+export const createPersonController = (db: PersonDatabase) => ({
+  getPersons: async (req: Request, res: Response) => {
     try {
       const persons = await db.getAll();
       res.status(200).json(persons);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(500).json({ error: error.message });
-        return;
-      }
-      res.status(500).json({ error: { message: "Internal server error" } });
+      res.status(500).json({ error: error instanceof Error ? error.message : "Internal server error" });
     }
-  };
+  },
 
-export const addPerson =
-  (db: PersonDatabase) => async (req: Request, res: Response) => {
+  addPerson: async (req: Request, res: Response) => {
     const { name, personalNumber, city } = req.body;
     const id = uuidv4();
-    const PersonToRegister = {
-      id,
-      name,
-      personalNumber,
-      city,
-    };
-    const result = personSchema.safeParse(PersonToRegister);
-    if (!result.success || !isValidLuhn(PersonToRegister.personalNumber)) {
+    const personToRegister = { id, name, personalNumber, city };
+    
+    const result = personSchema.safeParse(personToRegister);
+    if (!result.success || !isValidLuhn(personToRegister.personalNumber)) {
       res.status(400).json({ error: { message: "Invalid input" } });
       return;
     }
 
     try {
-      await db.addPerson(PersonToRegister);
-      res.status(201).json(PersonToRegister.id);
+      await db.addPerson(personToRegister);
+      res.status(201).json(personToRegister.id);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(400).json({ error: error.message });
-        return;
-      }
-      res.status(500).json({ error: { message: "Internal server error" } });
+      res.status(400).json({ error: error instanceof Error ? error.message : "Internal server error" });
     }
-  };
+  },
 
-export const getPersonById =
-  (db: PersonDatabase) => async (req: Request, res: Response) => {
+  getPersonById: async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
       const person = await db.getPersonById(id);
       res.status(200).json(person);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(404).json({ error: error.message });
-        return;
-      }
-      res.status(500).json({ error: { message: "Internal server error" } });
+      res.status(404).json({ error: error instanceof Error ? error.message : "Internal server error" });
     }
-  };
+  },
 
-export const deletePersonById =
-  (db: PersonDatabase) => async (req: Request, res: Response) => {
+  deletePersonById: async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
       await db.deletePersonById(id);
       res.status(200).json({ message: "Person deleted successfully" });
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(404).json({ error: error.message });
-        return;
-      }
-      res.status(500).json({ error: { message: "Internal server error" } });
+      res.status(404).json({ error: error instanceof Error ? error.message : "Internal server error" });
     }
-  };
+  },
 
-export const updatePersonById =
-  (db: PersonDatabase) => async (req: Request, res: Response) => {
+  updatePersonById: async (req: Request, res: Response) => {
     const id = req.params.id;
-    const personToUpdate = {
-      name: req.body.name,
-      city: req.body.city,
-    };
+    const personToUpdate = { name: req.body.name, city: req.body.city };
 
     const result = updatePersonSchema.safeParse(personToUpdate);
     if (!result.success) {
@@ -94,10 +67,7 @@ export const updatePersonById =
       const updatedPerson = await db.putPersonById(id, personToUpdate);
       res.status(200).json(updatedPerson);
     } catch (error) {
-      if (error instanceof Error) {
-        res.status(404).json({ error: error.message });
-        return;
-      }
-      res.status(500).json({ error: { message: "Internal server error" } });
+      res.status(404).json({ error: error instanceof Error ? error.message : "Internal server error" });
     }
-  };
+  }
+});
--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -1,27 +1,22 @@
 import express from "express";
 import { PersonDatabase } from "../db/person-database";
-import {
-  addPerson,
-  deletePersonById,
-  getPersonById,
-  getPersons,
-  updatePersonById,
-} from "../controllers/registrationController";
+import { createPersonController } from "../controllers/registrationController";
 
 export function createRegistrationRouter(db: PersonDatabase) {
   return {
     getRouter() {
+      const personController = createPersonController(db)
       const router = express.Router();
 
-      router.get("/", getPersons(db));
+      router.get("/", personController.getPersons);
 
-      router.post("/", addPerson(db));
+      router.post("/", personController.addPerson);
 
-      router.get("/:id", getPersonById(db));
+      router.get("/:id", personController.getPersonById);
 
-      router.delete("/:id", deletePersonById(db));
+      router.delete("/:id", personController.deletePersonById);
 
-      router.put("/:id", updatePersonById(db));
+      router.put("/:id", personController.updatePersonById);
 
       return router;
     },
